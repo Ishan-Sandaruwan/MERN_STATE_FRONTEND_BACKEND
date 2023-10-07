81cc63e0b0d0ae52778e7951b5e68e77a2ec7a7d
W I Sandaruwan <138509167+Ishan-Sandaruwan@users.noreply.github.com>
2023-10-07 12:48:08 +0530

    connect to database

--- API/index.js
+++ API/index.js
@@ -1,9 +1,15 @@
 import express from "express";
-
-const app = express();
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config(); // Load environment variables from .env file
 const port = 3000;
 
-
+mongoose.connect(process.env.MONGO).then(()=>{
+  console.log("connected to the mongo db");
+}).catch((err)=>{
+  console.log(err);
+})
+const app = express();
 
 app.listen(port, () => {
   console.log(`server is running on port ${port} `);
