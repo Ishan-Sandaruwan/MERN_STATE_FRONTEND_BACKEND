1a4d4ef665cba96aefee54261baa8b14446de25c
W I Sandaruwan <138509167+Ishan-Sandaruwan@users.noreply.github.com>
2023-10-07 13:16:35 +0530

    create test api route

--- API/index.js
+++ API/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRouter from './routes/user.route.js';
 dotenv.config(); // Load environment variables from .env file
 const port = 3000;
 
@@ -8,9 +9,16 @@ mongoose.connect(process.env.MONGO).then(()=>{
   console.log("connected to the mongo db");
 }).catch((err)=>{
   console.log(err);
-})
+});
+
 const app = express();
 
 app.listen(port, () => {
   console.log(`server is running on port ${port} `);
 });
+
+app.get("/",(req,res)=>{
+  res.send("hello world");
+});
+
+app.use('/api/user',userRouter);
